9e8ffb89e2fd9c508f7606cf82e221b806423d38
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 16:23:20 +0800

    docs: unify @author tag to @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/main/java/org/apache/cxf/spring/boot/jaxrs/endpoint/EndpointApiTemplate.java
+++ src/main/java/org/apache/cxf/spring/boot/jaxrs/endpoint/EndpointApiTemplate.java
@@ -32,7 +32,7 @@ import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
 
 /**
  * TODO
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  */
 public class EndpointApiTemplate {
 	
@@ -53,7 +53,7 @@ public class EndpointApiTemplate {
 
 	/**
 	 * 为指定的addr发布Endpoint
-	 * @author [@Loong Wan](https://github.com/loong10k)
+	 * @author <a href="https://github.com/loong10k">Loong Wan</a>
 	 * @param addr
 	 * @param implementors
 	 * @return
@@ -64,7 +64,7 @@ public class EndpointApiTemplate {
 
 	/**
 	 * 为指定的addr发布Endpoint
-	 * @author [@Loong Wan](https://github.com/loong10k)
+	 * @author <a href="https://github.com/loong10k">Loong Wan</a>
 	 * @param addr
 	 * @param implementor
 	 * @param callback
@@ -101,7 +101,7 @@ public class EndpointApiTemplate {
 	
 	/**
 	 * 为指定的addr发布Endpoint
-	 * @author [@Loong Wan](https://github.com/loong10k)
+	 * @author <a href="https://github.com/loong10k">Loong Wan</a>
 	 * @param addr
 	 * @param callback
 	 * @param classes
@@ -138,7 +138,7 @@ public class EndpointApiTemplate {
 
 	/**
 	 * 销毁指定路径匹配的Endpoint
-	 * @author [@Loong Wan](https://github.com/loong10k)
+	 * @author <a href="https://github.com/loong10k">Loong Wan</a>
 	 * @param addr
 	 */
 	public void destroy(String addr) {
